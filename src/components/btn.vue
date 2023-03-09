627b8b71a74d4b70572a8616ad08c584f707c62a
unknown <raul.19070050@itsmotul.edu.mx>
2023-03-09 16:58:19 -0600

    vue

---
+++ src/components/btn.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+<button  @click="enviar()">
+	<slot></slot>
+</button>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://isclab.com.mx/personasuxd/&#39',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base:'./',
+})
